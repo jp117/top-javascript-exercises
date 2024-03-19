883b1277ee973d72e1711bcc04cf8ba293263ab0
johnparadise <john.paradise117@gmail.com>
2024-03-19 19:41:26 -0400

    Completed assignment 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let answer = 0;
+    let smaller;
+    let larger;
+    if (first < second) {
+        smaller = first;
+        larger = second;
+    } else {
+        smaller = second;
+        larger = first;
+    }
+    if (
+        first >= 0 &&
+        second >= 0 &&
+        Number.isInteger(first) &&
+        Number.isInteger(second)
+    ) {
+        for (let i = smaller; i <= larger; i++){
+            answer += i;
+        }
+        return answer;
+    } else {
+        return 'ERROR'
+    }
 
 };
 
