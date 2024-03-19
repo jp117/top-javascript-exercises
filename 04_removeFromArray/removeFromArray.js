f669f9dbed7174d13d9ca310f9444005e00c8692
johnparadise <john.paradise117@gmail.com>
2024-03-19 19:20:49 -0400

    Completed assignment 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (arg of args){
+        for (let i = array.length -1; i >= 0; i--){
+            if (array[i] === arg){
+                array.splice(i,1);
+            }
+        }
+    }
+        
+    return array;
 };
 
 // Do not edit below this line
