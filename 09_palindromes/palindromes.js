b3ada1fa2e17c86bcb598c3f63c7ed5e77a6f474
jp117 <john.paradise117@gmail.com>
2024-03-24 22:20:29 -0400

    Completed 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+//discard punctuation
+  const validChar = 'abcdefghijklmnopqrstuvwxyz01233456789'
+  
+  let validString = str.toLowerCase();
+  validString = validString.split('').filter((character) => validChar.includes(character)).join('');
+
+  const reverseString = validString.split('').reverse().join(''); 
+
+  return validString === reverseString;
 
 };
 
