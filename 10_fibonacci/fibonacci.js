6ff044270dcaf254d4794150c81422ac2439b490
jp117 <john.paradise117@gmail.com>
2024-03-25 06:41:38 -0400

    Completed 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let a = 1, b = 0, temp;
 
+  if (number < 0){
+    return "OOPS";
+  }
+
+  while (number > 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    number --;
+  }
+  return b;
 };
 
 // Do not edit below this line
