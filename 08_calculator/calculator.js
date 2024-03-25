8248e3be9e7a31e6df30137da1406cc15a91a655
jp117 <john.paradise117@gmail.com>
2024-03-24 21:39:09 -0400

    Completed 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;	
+};
+
+const sum = function(array) {
+  return array.reduce((sum , current) => 
+   sum + current
+  , 0)
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(array) {
+  return array.reduce((mult, current) =>  mult * current) 
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a==0 || a == 1){
+    return 1;
+  } else {
+    return a * factorial( a-1);
+  }
 };
 
 // Do not edit below this line
