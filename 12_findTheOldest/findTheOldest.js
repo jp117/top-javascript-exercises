dc6c4dfe9e5c36fb7a9de835bf9aff2735fe030c
jp117 <john.paradise117@gmail.com>
2024-03-25 20:14:19 -0400

    Completed 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestAge;
+  let oldestPerson;
+  let currentAge;
+  for (person of people) {
+    console.log(person);
+    if (!oldestAge) {
+      oldestAge = getAge(
+        person.yearOfBirth,
+        person.yearOfDeath
+      );
+      oldestPerson = person;
+    } else {
+      currentAge = getAge(
+        person.yearOfBirth,
+        person.yearOfDeath
+      );
+      if (currentAge > oldestAge){
+        oldestPerson = person;
+        oldestAge = currentAge;
+      }
+    }
+  }
+  return oldestPerson;
 };
+
+const getAge = function(birthYear, deathYear){
+  if (!deathYear){
+    (deathYear = new Date().getFullYear());
+  } 
+  return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
